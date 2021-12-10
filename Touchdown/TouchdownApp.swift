fad2f5ae6fb7e36da8239a0069b3cbe8f422c828
David Ham <davidham80@yahoo.com>
2021-12-10 13:25:03 -0600

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by David Ham on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
